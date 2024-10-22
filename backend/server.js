252a6be8664a9f4b6d33ba42c1a25d01ec10be94
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-10-23 00:03:50 +0300

    Connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import { configDotenv } from "dotenv";
 import express from "express";
 import { authRoutes } from "./routes/authRoutes.js";
+import { connectMongoDB } from "./db/connect.MongoDB.js";
 configDotenv(); // configure the dotenv
 
 const app = express();
@@ -11,4 +12,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  connectMongoDB();
 });
