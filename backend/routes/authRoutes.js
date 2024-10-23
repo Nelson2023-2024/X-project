dbba3abee973158f3b8f75247cb3c2bb05fc479b
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-10-23 07:37:44 +0300

    Logout route completed

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -90,6 +90,14 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.post("/logout", async (req, res) => {});
+router.post("/logout", async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout successfully" });
+  } catch (error) {
+    console.log(`Error in the Logout Route`, error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 export { router as authRoutes };
