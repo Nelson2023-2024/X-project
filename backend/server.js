88fd0fcbb61bab7b70a06a7087598c8419ff6716
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-10-27 16:09:47 +0300

    Revert "Finished the user profile route"
    This reverts commit 7f3ea1890ea2597cf22a1e900c66ba3e2ab544d8.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import { configDotenv } from "dotenv";
 import express from "express";
+<<<<<<< HEAD
 import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
 
+=======
+>>>>>>> parent of 7f3ea18 (Finished the user profile route)
 import { authRoutes } from "./routes/authRoutes.js";
-import { userRoutes } from "./routes/user.routes.js";
-
 import { connectMongoDB } from "./db/connect.MongoDB.js";
+<<<<<<< HEAD
 import { postRoutes } from "./routes/postRoutes.js";
 
 configDotenv(); // configure the dotenv'
@@ -16,6 +18,10 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+=======
+import cookieParser from "cookie-parser";
+configDotenv(); // configure the dotenv
+>>>>>>> parent of 7f3ea18 (Finished the user profile route)
 
 const app = express();
 const PORT = process.env.PORT;
@@ -24,8 +30,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+<<<<<<< HEAD
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+=======
+>>>>>>> parent of 7f3ea18 (Finished the user profile route)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
