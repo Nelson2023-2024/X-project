47c8d82368a2d4776c687e272e30f8ed238f6433
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-10-27 16:46:56 +0300

    Like and unlike route completed

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,14 +1,15 @@
-import { Router } from "express";
-import { v2 as cloudinary } from "cloudinary";
-import { protectRoute } from "../middleware/protectRoute.js";
-import { Post } from "../models/post.model.js";
-import { User } from "../models/user.model.js";
+import { Router } from 'express';
+import { v2 as cloudinary } from 'cloudinary';
+import { protectRoute } from '../middleware/protectRoute.js';
+import { Post } from '../models/post.model.js';
+import { User } from '../models/user.model.js';
+import { Notification } from '../models/notification.model.js';
 
 const router = Router();
 
 router.use(protectRoute);
 
-router.post("/create", async (req, res) => {
+router.post('/create', async (req, res) => {
   try {
     let { text } = req.body;
     let { img } = req.body;
@@ -16,9 +17,9 @@ router.post("/create", async (req, res) => {
     const userId = req.user._id.toString();
 
     const user = await User.findById(userId);
-    if (!user) return res.status(404).json({ error: "User not found" });
+    if (!user) return res.status(404).json({ error: 'User not found' });
     if (!text && !img)
-      return res.status(400).json({ error: "Post must have text or image" });
+      return res.status(400).json({ error: 'Post must have text or image' });
 
     if (img) {
       const uploadedResponse = await cloudinary.uploader.upload(img);
@@ -28,29 +29,64 @@ router.post("/create", async (req, res) => {
     const newPost = new Post({
       user: userId,
       text,
-      img
+      img,
     });
 
     await newPost.save();
-    res.status(201).json({ message: "Post created successfull", newPost });
+    res.status(201).json({ message: 'Post created successfull', newPost });
   } catch (error) {
     console.log(`Error in create Post Route:`, error.message);
     res.sendStatus(500);
   }
 });
-router.post("/like-unlike/:id", async (req, res) => {});
-router.post("/comment/:postId", async (req, res) => {
+router.post('/like-unlike/:id', async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id: postId } = req.params;
+
+    const post = await Post.findById(postId);
+
+    if (!post) return res.status(404).json({ error: 'Post not found' });
+
+    const userLikedPost = post.likes.includes(userId);
+
+    //if user already liked the post unlike it
+    if (userLikedPost) {
+      //unlike
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: 'Post unliked sucessfully' });
+    } else {
+      //like
+      post.likes.push(userId);
+      await post.save();
+
+      //send notification
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: 'like',
+      });
+      await notification.save();
+
+      res.status(200).json({ message: 'Post liked succesffuly' });
+    }
+  } catch (error) {
+    console.log('Error in like-unlike controller', error);
+    res.sendStatus(500);
+  }
+});
+router.post('/comment/:postId', async (req, res) => {
   try {
     const { text } = req.body;
     const { postId } = req.params;
 
     const userId = req.user._id;
 
-    if (!text) return res.status(400).json({ error: "Text field is required" });
+    if (!text) return res.status(400).json({ error: 'Text field is required' });
 
     const post = await Post.findById(postId);
 
-    if (!post) return res.status(404).json({ error: "Post not found" });
+    if (!post) return res.status(404).json({ error: 'Post not found' });
 
     const comment = { user: userId, text };
 
@@ -59,35 +95,35 @@ router.post("/comment/:postId", async (req, res) => {
 
     res.status(201).json(post);
   } catch (error) {
-    console.log("Error in deletePost", error);
+    console.log('Error in deletePost', error);
     res.sendStatus(500);
   }
 });
-router.delete("/:postId", async (req, res) => {
+router.delete('/:postId', async (req, res) => {
   try {
     const { postId } = req.params;
     const post = await Post.findById(postId);
 
-    if (!post) return res.status(404).json({ error: "Post not found" });
+    if (!post) return res.status(404).json({ error: 'Post not found' });
 
     //check if we are the owner of the post
     if (post.user.toString() !== req.user._id.toString()) {
       return res
         .status(401)
-        .json({ error: "You are not authorized to delete this post" });
+        .json({ error: 'You are not authorized to delete this post' });
     }
 
     //if post has an image delete it from cloudinary
     if (post.img) {
-      const imgId = post.img.split("/").pop().split(".")[0];
+      const imgId = post.img.split('/').pop().split('.')[0];
       await cloudinary.uploader.destroy(imgId);
     }
 
     await Post.findByIdAndDelete(postId);
 
-    res.status(200).json({ message: "Post deleted successfully", post });
+    res.status(200).json({ message: 'Post deleted successfully', post });
   } catch (error) {
-    console.log("Error in deletePost", error);
+    console.log('Error in deletePost', error);
     res.sendStatus(500);
   }
 });
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,21 @@
-import { configDotenv } from "dotenv";
-import express from "express";
-<<<<<<< HEAD
-import { v2 as cloudinary } from "cloudinary";
-import cookieParser from "cookie-parser";
+import { configDotenv } from 'dotenv';
+import express from 'express';
+import { v2 as cloudinary } from 'cloudinary';
+import cookieParser from 'cookie-parser';
 
-=======
->>>>>>> parent of 7f3ea18 (Finished the user profile route)
-import { authRoutes } from "./routes/authRoutes.js";
-import { connectMongoDB } from "./db/connect.MongoDB.js";
-<<<<<<< HEAD
-import { postRoutes } from "./routes/postRoutes.js";
+import { authRoutes } from './routes/authRoutes.js';
+import { userRoutes } from './routes/user.routes.js';
+
+import { connectMongoDB } from './db/connect.MongoDB.js';
+import { postRoutes } from './routes/postRoutes.js';
 
 configDotenv(); // configure the dotenv'
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-=======
-import cookieParser from "cookie-parser";
-configDotenv(); // configure the dotenv
->>>>>>> parent of 7f3ea18 (Finished the user profile route)
 
 const app = express();
 const PORT = process.env.PORT;
@@ -29,12 +23,9 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-<<<<<<< HEAD
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-=======
->>>>>>> parent of 7f3ea18 (Finished the user profile route)
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
