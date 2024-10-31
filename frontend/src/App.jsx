f3fa8f20f1dd774e632b401cd67e564e3d852e32
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-10-31 09:50:25 +0300

    React Router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 // import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/auth/Home/HomePage';
+import SignupPage from './pages/auth/Signup/SignupPage';
+import LoginPage from './pages/auth/Login/LoginPage';
 
 const App = () => {
-  return <div className="text-5xl text-red-400">App</div>;
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
