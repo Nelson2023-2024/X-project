214dc23edcc727e412d66446ee61e02a874e9567
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-10-31 19:16:04 +0300

    Finished styling the sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/auth/Home/HomePage';
 import SignupPage from './pages/auth/Signup/SignupPage';
 import LoginPage from './pages/auth/Login/LoginPage';
+import Sidebar from './components/common/Sidebar';
 
 const App = () => {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      {/* Common component as it's not part of routes */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
