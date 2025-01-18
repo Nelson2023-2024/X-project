e3a50a7f2d70001cba461d935700ee952a36b35d
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-01-19 01:58:32 +0300

    Authenticated functionality completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,45 @@
 // import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage';
-import SignupPage from './pages/auth/Signup/SignupPage';
-import LoginPage from './pages/auth/Login/LoginPage';
-import Sidebar from './components/common/Sidebar';
-import RightPanel from './components/common/RightPanel';
-import NotificationPage from './pages/Notifications/NotificationPage';
-import ProfilePage from './pages/Profile/ProfilePage';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
+import SignupPage from "./pages/auth/Signup/SignupPage";
+import LoginPage from "./pages/auth/Login/LoginPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/Notifications/NotificationPage";
+import ProfilePage from "./pages/Profile/ProfilePage";
 
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 
 const App = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const response = await fetch("/api/auth/me");
+        const data = response.json();
+
+        if (!response.ok || data.error) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        console.log("Authuser:", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  console.log("Authuser is here:", data);
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    )
+  }
   return (
     <div className="flex max-w-6xl mx-auto">
       <Sidebar />
