aef5a3f610cfdd96c25f0d0c01a617b5e485699a
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-01-19 14:46:11 +0300

    Fetched the approprate values for the sidebar and displayed them for the authenticated user

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -3,54 +3,53 @@ import XSvg from "../svgs/X";
 import { MdHomeFilled } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const Sidebar = () => {
+  const queryClient = useQueryClient();
 
-  const navigate = useNavigate()
-  const queryClient = useQueryClient()
-
-
-  const data = {
+  let data = {
     fullName: "John Doe",
     username: "johndoe",
     profileImg: "/avatars/boy1.png",
   };
 
-
-
-  const { mutate: logout, isPending, isError, error } = useMutation({
+  const { mutate: logout } = useMutation({
     mutationFn: async () => {
-      const response = await fetch('/api/auth/logout', {
+      const response = await fetch("/api/auth/logout", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-      })
+      });
 
-      const data = response.json()
+      const data = response.json();
 
       if (!response.ok || data.error) {
         throw new Error(data.error || "Something went wrong");
       }
 
-      return data
+      return data;
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["authUser"] })
-      queryClient.setQueryData(['authUser'], null)
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+      queryClient.setQueryData(["authUser"], null);
     },
     onError: (error) => {
-      toast.error(error.message)
-    }
+      toast.error(error.message);
+    },
   });
 
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+
+  console.log("data in sidebar", authUser);
+
   function handleLogout(event) {
-    event.preventDefault() //prevents navigation to that page
-    logout()
+    event.preventDefault(); //prevents navigation to that page
+    logout();
   }
 
   return (
@@ -81,7 +80,7 @@ const Sidebar = () => {
 
           <li className="flex justify-center md:justify-start">
             <Link
-              to={`/profile/${data?.username}`}
+              to={`/profile/${authUser?.username}`}
               className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer"
             >
               <FaUser className="w-6 h-6" />
@@ -91,24 +90,26 @@ const Sidebar = () => {
         </ul>
         {data && (
           <Link
-            to={`/profile/${data.username}`}
+            to={`/profile/${authUser?.username}`}
             className="mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full"
           >
             <div className="avatar hidden md:inline-flex">
               <div className="w-8 rounded-full">
-                <img src={data?.profileImg || "/avatar-placeholder.png"} />
+                <img src={authUser?.profileImg || "/avatar-placeholder.png"} />
               </div>
             </div>
             <div className="flex justify-between flex-1">
               <div className="hidden md:block">
                 <p className="text-white font-bold text-sm w-20 truncate">
-                  {data?.fullName}
+                  {authUser?.fullName}
                 </p>
-                <p className="text-slate-500 text-sm">@{data?.username}</p>
+                <p className="text-slate-500 text-sm">@{authUser?.username}</p>
               </div>
             </div>
-            <BiLogOut className="w-5 h-5 cursor-pointer" onClick={handleLogout} />
-
+            <BiLogOut
+              className="w-5 h-5 cursor-pointer"
+              onClick={handleLogout}
+            />
           </Link>
         )}
       </div>
